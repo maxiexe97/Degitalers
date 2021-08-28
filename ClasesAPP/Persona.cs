2ee878ad3187d0bca458a5b759ee6a824095b0c2
maxi <maxi@email.com>
2021-08-28 16:31:25 -0300

    intro oop

---
+++ ClasesAPP/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAPP
+{
+    class Persona
+    {
+       private string Nombre;
+       private string Apellido;
+       private string dni;
+
+        public void setDni(string dni_param)
+        {
+            dni = dni_param;
+        }
+        public string getDni()
+        {
+            return dni;
+        }
+
+        public void saludar()
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
---
+++ ClasesAPP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Saludo = "Hola";
+            /*Persona persona1 = new Persona();
+            Persona persona2 = persona1;
+            persona1.Nombre = "María";
+            persona1.Apellido = "Perez";
+
+            Console.WriteLine($"Hola mi nombre es {persona1.Nombre} {persona1.Apellido} ");*/
+           // desde la clase Program
+            Persona p2 = new Persona();
+            //p2.saludar();
+
+            Persona p3 = new Persona();
+            p3.setDni("40276025");
+            Console.WriteLine("El DNI es: {0}", p3.getDni());
+
+            Console.ReadKey();
+        }
+    }
+}
