1b83c0b0062b002e0c95b54418d84e9e6e9dcb52
maxi <maxi@email.com>
2021-08-29 19:24:02 -0300

    metodos get/set

--- ClasesAPP/Persona.cs
+++ ClasesAPP/Persona.cs
@@ -8,22 +8,49 @@ namespace ClasesAPP
 {
     class Persona
     {
-       private string Nombre;
-       private string Apellido;
+     
+       public string Nombre { get; set; }
+       public string Apellido { get; set; }
+       
+        private int edad; 
        private string dni;
 
-        public void setDni(string dni_param)
+       /* public void setDni(string dni_param)
         {
             dni = dni_param;
-        }
+        }*/
         public string getDni()
         {
             return dni;
         }
 
-        public void saludar()
+        public void setEdad(int edad_param)
         {
-            Console.WriteLine("Hola Mundo!");
+            edad = edad_param;
+           /* if (edad_param < 18)
+            {
+                Console.WriteLine("No tenes edad suficiente para ingresar");
+                edad = 0;
+            }
+            else
+            {
+                Console.WriteLine("Tienes la edad suficiente");
+            }*/
+
         }
+        public int getEdad()
+        {
+            return edad;
+        }
+
+        public Persona(string dni_param)
+        {
+            dni = dni_param;
+        }
+
+
+
+
+
     }
 }
--- ClasesAPP/Program.cs
+++ ClasesAPP/Program.cs
@@ -10,24 +10,53 @@ namespace ClasesAPP
     {
         static void Main(string[] args)
         {
-            string Saludo = "Hola";
+            Persona p1 = new Persona("11111111");
+            Persona p2 = new Persona("22222222");
+            Persona p3 = new Persona("33333333");
+
+            p1.Nombre = "Juan";
+            p1.setEdad(20);
+            p2.Nombre = "Ramiro";
+            p2.setEdad(24);
+            p3.Nombre = "Lucia";
+            p3.setEdad(33);
+
+            Console.WriteLine("Nombre: {0}, Edad: {1}, DNI: {2}", p1.Nombre, p1.getEdad(), p1.getDni());
+            Console.WriteLine("Nombre: {0}, Edad: {1}, DNI: {2}", p2.Nombre, p2.getEdad(), p2.getDni());
+            Console.WriteLine("Nombre: {0}, Edad: {1}, DNI: {2}", p3.Nombre, p3.getEdad(), p3.getDni());
+
+            //string Saludo = "Hola";
             /*Persona persona1 = new Persona();
             Persona persona2 = persona1;
             persona1.Nombre = "María";
             persona1.Apellido = "Perez";
 
             Console.WriteLine($"Hola mi nombre es {persona1.Nombre} {persona1.Apellido} ");*/
-           // desde la clase Program
-            Persona p2 = new Persona();
+            // desde la clase Program
+            //Persona p2 = new Persona();
+
             //p2.saludar();
 
-            Persona p3 = new Persona();
+            /*Persona p3 = new Persona();
             p3.setDni("40276025");
-            Console.WriteLine("El DNI es: {0}", p3.getDni());
+            Console.WriteLine("El DNI es: {0}", p3.getDni());*/
+
+            // Console.WriteLine("Bienvenido a mi cervecería!");
+            // p3.setEdad(14);
+            /*if (p3.getEdad() < 18)
+            {
+                Console.WriteLine("No tenes edad suficiente para ingresar");
+            }
+            else
+            {
+                Console.WriteLine("Tienes la edad suficiente");
+            }
+*/
+            //p3.Nombre="Maxi";
+            //Console.WriteLine("Bienvenido a mi cervecería {0}!",p3.Nombre);
 
             Console.ReadKey();
-            ////7
-            //RAMA OOP2
+            
         }
     }
 }
